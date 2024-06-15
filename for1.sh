2e6d38032c1e27d97f326518f1d0ec4d87600611
Samnejad92 <112156738+Samnejad92@users.noreply.github.com>
2024-06-15 18:00:27 +0330

    Add files via upload

---
+++ for1.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+for word in "linux" "is" "perfect" "and" "windows?" "mmm," "not" "bad."
+do
+    echo -n "$word "
+done
+echo
---
+++ for2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+for word in $(cat list)
+do
+    echo $word
+done
